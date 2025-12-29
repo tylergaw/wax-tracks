2a7049d2a0b1d65e914395b37b443754218f6aa1
Tyler Gaw <me@tylergaw.com>
2025-12-29 18:12:37 -0500

    Abstracts system context

--- src/shared.js
+++ src/shared.js
@@ -23,3 +23,24 @@ export function getFlag(args, flagName, defaultValue) {
   }
   return flagValue;
 }
+
+export const systemInstructions = [
+  "You are a vinyl record color expert. Given a JSON string that contains and id and human-written description of the color, pattern, and texture of records, you should respond with machine-readable colors in JSON format. Keep in mind all of the following:",
+  "Make sure your response is always in the shape; { records: [ {<record>} ]} ",
+  "The user is going to send you a stringified JSON array, you need to parse it as JSON then iterate over each record in the array",
+  "In the JSON please include the id value for each record",
+  "Please omit the 'id' value from the JSON description member",
+  "In the JSON for each please include a 'humanReadableColor' field that contains color/text/pattern information in a way that makes sense to humans",
+  "Please treat 'translucent' as part of the human color if it's in the description as well as listing it as the pattern/texture.",
+  "The presence of 'with' or '&' usually indicate a combination of colors, those tokens should go in the human readable color.",
+  "Make sure every JSON object includes the fields; id, description, humanReadableColor, cssReadableColors, pattern, texture. If any don't have a value, set them to null.",
+  "cssReadableColors should only include valid css/js colors, no other tokens.",
+  "'marble', 'marbled' should never be included in css_readable_colors.",
+  "Don't convert the word 'and' into an '&'.",
+  "Make values for cssReadableColors lowercase.",
+  "Make values for pattern and texture lowercase.",
+  "'Clear' should be considered a texture NOT a color.",
+  "'sunburst' should be considered a pattern.",
+  "The description should be the value of the description member as you receive it.",
+  "Most important, if you don't find a cssReadableColor, pattern, or texture, don't include an object for it in your response.",
+];
